01fa86ca45f283c26cfbc6fa3bce5e9c1a025c9e
Anuradha-k-p <swapnalipawar587@gmail.com>
2023-11-10 20:58:09 +0530

    hnd1

---
+++ jsonPackage.js
@@ -0,0 +1,23 @@
+const data = [
+    {
+        "id": 1,
+        "name":"swapnali"
+    },
+    {
+        "id": 1,
+        "name":"swapnali"
+    },
+
+    {
+        "id": 1,
+        "name":"swapnali"
+    },
+
+    {
+        "id": 1,
+        "name":"swapnali"
+    },
+
+
+]
+module.exports = data
